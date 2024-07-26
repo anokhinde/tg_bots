2c217573632a5ebd3a9c7a62fe5d6f452f826caa
Dmitrii Anokhin <anokhinde@gmail.com>
2024-07-26 13:27:35 +0300

    working wersion

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+import logging
 import telegram.ext
 
+# модуль ведения журнала логов
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+# читаем API KEY из файла
 with open('venv/config.txt', 'r') as f:
     TOKEN = f.read()
 
 def start(update, context):
-    update.message.reply_text('Hello world')
+    update.message.reply_text("Привет! Я твой служебный бот - Selenas_memoris_bot")
 
 def help(update, context):
-    update.message.reply_text ('''
+    update.message.reply_text('''
         Команды бота:
                                
         /start -> Welcome message
@@ -22,13 +30,19 @@ def content(update, context):
 def contact(update, context):
     update.message.reply_text('message2')
 
-updater =telegram.ext.Updater(TOKEN, use_context=True)
-disp = updater.dispatcher
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, 
+                                   text="Sorry, I didn't understand that command.")
+    
+if __name__ == '__main__':
+    # создание экземпляра бота через `ApplicationBuilder`
+    updater = telegram.ext.Updater(TOKEN, use_context=True)
+    disp = updater.dispatcher
 
-disp.add_handler(telegram.ext.CommandHandler("start", start))
-disp.add_handler(telegram.ext.CommandHandler("help", help))
-disp.add_handler(telegram.ext.CommandHandler("content", content))
-disp.add_handler(telegram.ext.CommandHandler("contact", contact))
+    disp.add_handler(telegram.ext.CommandHandler("start", start))
+    disp.add_handler(telegram.ext.CommandHandler("help", help))
+    disp.add_handler(telegram.ext.CommandHandler("content", content))
+    disp.add_handler(telegram.ext.CommandHandler("contact", contact))
 
-updater.start_polling()
-updater.idle()
+    updater.start_polling()
+    updater.idle()
